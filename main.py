100d951ba76f6fae4841fd07fc809068d19cab67
calebrauxa <moore.caleb@gmail.com>
2024-01-30 12:08:39 -0800

    folder version working

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 import whisper
+import argparse
+import os
 
-model = whisper.load_model("base")
-result = model.transcribe("test.m4a")
-print(result["text"])
+def main():
+    parser = argparse.ArgumentParser(description='Voice Memo - (c) 2024 Caleb Moore') 
+    # parser.add_argument('audiofilename', type=str, help='Name of the audio file to process')
+    parser.add_argument('audiofolder', type=str, help='Name of the folder to process')
+    args = parser.parse_args()
+    
+    # audiofile = args.audiofilename
+    audiofolder = args.audiofolder
+    model = whisper.load_model("base")
+
+    # loop through each file inside of the audiofolder file folder and transcribe it
+    for file in os.listdir(audiofolder):
+        audiofile = os.path.join(audiofolder, file)
+        print(f"Processing file: {audiofile}")
+        result = model.transcribe(audiofile, fp16=False)
+        language = "en"
+        print(file + result["text"])
+
+
+    # print(f"Processing file: {audiofile}")
+
+    # result = model.transcribe(audiofile, fp16=False)
+    # language = "en"
+
+    # print(result["text"])
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Whisper local w python notes
+
+- created python environment in vscode using VENV
+  - Python: Create Environment > Venv > Python 3.11.7
+- check that pip is up to date
+  - python3 -m pip install --upgrade pip
+- install whisper to environment
+  - python3 -m pip install -U openai-whisper
+
+worked by pressing play with hard coded test.m4a 
+
+----
+
+- created alias for easier command line usage, in .zshrc in ~/
+  - alias vm="/Users/caleb/git/vm/.venv/bin/python /Users/caleb/git/vm/main.py"
+- added argument for audiofile so i can run in terminal like
+  - vm test.m4a
+- parseargs also has built in --help and -h that prints:
+
+    usage: main.py [-h] audiofile
+
+    Voice Memo - (c) 2024 Caleb Moore
+
+    positional arguments:
+      audiofile   Name of the audio file to process
+
+    options:
+      -h, --help  show this help message and exit
+
+- added audiofolder argument that will process each file in a folder path with command:
+    vm test/ 
+- added fp16=False to model.transcribe to avoid that error in terminal about needing to use fp32
+
+TODO:
++ option of a single file OR the folder
